f15beac0799a61b6c25988e082d5b85316953479
aliev008 <hfe.as@yandex.ru>
2022-07-19 03:55:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Section1 from "./components/section1/section1";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Section1 />
+    </div>
+  );
+}
+
+export default App;
