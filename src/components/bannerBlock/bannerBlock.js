7ee4234d8cce6e4417dccabcbde05ad9ec35cce2
aliev008 <hfe.as@yandex.ru>
2022-08-12 13:17:18 +0300

    refactor: added css modules

--- src/components/bannerBlock/bannerBlock.js
+++ src/components/bannerBlock/bannerBlock.js
@@ -1,15 +1,15 @@
-import "./style.scss";
+import style from "./style.module.scss";
 
 import React from "react";
 
 const BannerBlock = () => (
-  <section className="banner-block">
-    <div className="banner">
-      <div className="banner-text">
+  <section className={style.banner_block}>
+    <div className={style.banner}>
+      <div className={style.banner_text}>
         <p>The title of the component</p>
         <h1>Very visible component to promote something.</h1>
       </div>
-      <button className="banner-btn">Book an Appointment</button>
+      <button className={style.banner_btn}>Book an Appointment</button>
     </div>
   </section>
 );
