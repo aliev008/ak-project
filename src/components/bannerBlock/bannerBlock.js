2d25c9c095ec74d8560be9016bb574902c5225ca
aliev008 <hfe.as@yandex.ru>
2022-08-15 15:53:05 +0300

    fix: renamed classes with bem methodology

--- src/components/bannerBlock/bannerBlock.js
+++ src/components/bannerBlock/bannerBlock.js
@@ -3,13 +3,13 @@ import style from "./style.module.scss";
 import React from "react";
 
 const BannerBlock = () => (
-  <section className={style.banner_block}>
-    <div className={style.banner}>
-      <div className={style.banner_text}>
+  <section className={style.bannerBlock}>
+    <div className={style.banner__container}>
+      <div className={style.banner__text}>
         <p>The title of the component</p>
         <h1>Very visible component to promote something.</h1>
       </div>
-      <button className={style.banner_btn}>Book an Appointment</button>
+      <button className={style.banner__btn}>Book an Appointment</button>
     </div>
   </section>
 );
