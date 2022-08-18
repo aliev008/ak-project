a5dc96fdc5e0cf2f6b752dcdbcd64a29a46bb54b
aliev008 <hfe.as@yandex.ru>
2022-08-18 16:22:58 +0300

    fix: several styles fixes

--- src/components/servicesBlock/servicesBlock.js
+++ src/components/servicesBlock/servicesBlock.js
@@ -9,38 +9,48 @@ import card5 from "../../assets/cards/Back-end Development.svg";
 import Card from "./card";
 
 const ServicesBlock = () => {
+
   const cards = [
-    [
-      card1,
-      "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris malesuada ornare.",
-    ],
-    [
-      card2,
-      "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris.",
-    ],
-    [
-      card3,
-      "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas.",
-    ],
-    [
-      card4,
-      "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris malesuada ornare.",
-    ],
-    [
-      card5,
-      "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris.",
-    ],
+    {
+      imgSrc: card1,
+      text:
+        "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris malesuada ornare.",
+    },
+
+    {
+      imgSrc: card2,
+      text:
+        "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris.",
+    },
+
+    {
+      imgSrc: card3,
+      text:
+        "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas.",
+    },
+
+    {
+      imgSrc: card4,
+      text:
+        "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris malesuada ornare.",
+    },
+
+    {
+      imgSrc: card5,
+      text:
+        "Lorem built ipsum dolor sit amet. Eget vestibulum volutpat id nibh felis dignissim. Aliquet porta ultrices orci metus. Ut aenean pretium. Mollis magna hac amet maecenas mauris.",
+    },
   ];
 
   return (
     <section className={styles.services_block}>
       <div className={styles.services_block__grid}>
-        {cards.map((elem, index) => (
+        {cards.map((card, index) => (
           <Card
             key={index}
             className={styles.services_block__card}
-            imgSrc={elem[0]}
-            text={elem[1]}
+            imgSrc={card.imgSrc}
+            text={card.text}
           />
         ))}
       </div>
