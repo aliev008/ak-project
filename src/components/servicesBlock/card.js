60a2d24df1463c78947cd79a955eedd208e30987
aliev008 <hfe.as@yandex.ru>
2022-08-20 18:04:30 +0300

    fix: added responsiveness to services-block

--- src/components/servicesBlock/card.js
+++ src/components/servicesBlock/card.js
@@ -5,8 +5,10 @@ const Card = (props) => {
   return (
     <div className={props.className}>
       <img src={props.imgSrc} alt="card"></img>
-      <p className={styles.services_block__title}>Front-end development</p>
-      <p>{props.text}</p>
+      <div className={styles.services_block__text}>
+        <p className={styles.services_block__title}>Front-end development</p>
+        <p>{props.text}</p>
+      </div>
       <div className={styles.services_block__button}>
         <a href="#main" target="_blank">
           <p> Read More /</p>
